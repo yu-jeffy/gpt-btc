31287bf9f73e5d78116977a3411c29d377d38561
Jeff Yu <105130385+yu-jeffy@users.noreply.github.com>
2023-11-19 22:44:35 +0000

    news scrape redudancy fix, added main fnct

--- news_scrape.py
+++ news_scrape.py
@@ -67,6 +67,69 @@ for article in scraped_articles:
 
 
 
+# Function to analyze an article with GPT-4
+def analyze_article_with_gpt(article_text):
+    try:
+        # System prompt
+        system_prompt = (
+        "You are a highly knowledgeable assistant with expertise in Bitcoin and cryptocurrency markets. "
+        "Your task is to analyze news articles related to Bitcoin. For each article, you will provide a concise summary. "
+        "After summarizing, you will rate the sentiment of the article, its relevance to the Bitcoin and cryptocurrency markets, "
+        "and its overall importance. Rate each of these three aspects on a scale from 0 to 100, where 0 is the lowest and 100 is the highest. "
+        "Your responses should be factual, unbiased, and based solely on the content of the article. "
+        "Respond in a structured format that includes the summary followed by the ratings for sentiment, relevance, and importance. "
+        "For example: 'Summary: [Your summary here]. Sentiment: [0-100], Relevance: [0-100], Importance: [0-100].' "
+        "Do not have any text following the integer rating for Sentiment, Relevance, nor Importance."
+        "Avoid speculation and provide analysis based on the information available in the article."
+        )
+
+        # Structured prompt for analysis
+        analysis_prompt = (
+            f"Analyze the following Bitcoin-related article and provide a summary, "
+            f"then rate its sentiment, market relevance, and importance on a scale from 0 to 100. "
+            f"Do not have any text following the integer rating for Sentiment, Relevance, nor Importance."
+            f"We will be parsing the response with a predetermined format, STRICTLY follow the format"
+            f"Respond in the exact following format: "
+            f"Summary: [Your summary here] "
+            f"Sentiment: [0-100], Relevance: [0-100], Importance: [0-100].\n\n{article_text}"
+        )
+
+        # Create the chat completion
+        response = openai.chat.completions.create(
+            model="gpt-4-1106-preview",
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": analysis_prompt}
+            ],
+            temperature=0.3,
+            max_tokens=1200
+        )
+
+        # Assuming the response follows the format, we extract the summary and ratings
+        response_content = response.choices[0].message.content
+        # print(response.choices[0].message.content)
+        summary = response_content.split('Summary: ')[1].split(' Sentiment: ')[0].strip()
+        
+        # Extracting sentiment, relevance, and importance, handling extra text
+        sentiment_str = response_content.split('Sentiment: ')[1].split(',')[0].split(' ')[0].strip()
+        relevance_str = response_content.split('Relevance: ')[1].split(',')[0].split(' ')[0].strip()
+        importance_str = response_content.split('Importance: ')[1].split('.')[0].split(' ')[0].strip()
+
+        # Converting string to integer, handling any non-numeric characters
+        sentiment = int(''.join(filter(str.isdigit, sentiment_str)))
+        relevance = int(''.join(filter(str.isdigit, relevance_str)))
+        importance = int(''.join(filter(str.isdigit, importance_str)))
+
+        return {
+            'summary': summary,
+            'sentiment': sentiment,
+            'relevance': relevance,
+            'importance': importance
+        }
+    except Exception as e:  # Catching a general exception for simplicity
+        print(f"An error occurred: {str(e)}")
+        return None
+
 # Function to analyze and store the results for all articles
 def analyze_and_store_articles(articles):
     for article in articles:
@@ -77,7 +140,7 @@ def analyze_and_store_articles(articles):
 # Analyze all articles once and store the results
 analyze_and_store_articles(scraped_articles)
 
-# Now you can print the summaries and ratings from the stored results
+# Print the summaries and ratings from the stored results
 for article in scraped_articles:
     print(f"Title: {article['title']}")
     print(f"Summary: {article['summary']}")
@@ -108,3 +171,14 @@ def save_results_to_file(articles, results_directory='results'):
 
 # Save the results to a file with a timestamped filename
 save_results_to_file(scraped_articles)
+
+def main():
+    articles = fetch_news_articles()
+    print(articles)
+    scraped_articles = scrape_all_articles(articles)
+    analyze_and_store_articles(scraped_articles)
+    print_scraped_articles(scraped_articles)  # You would define this function to print article snippets
+    save_results_to_file(scraped_articles)
+
+if __name__ == '__main__':
+    main()
