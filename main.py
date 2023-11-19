451d3adccea762dbe488d1664e9f85451cf2bd1b
Jeff Yu <105130385+yu-jeffy@users.noreply.github.com>
2023-11-19 22:27:35 +0000

    news scrape redundancy fix, filename fix in main

--- main.py
+++ main.py
@@ -1,3 +1,86 @@
 import price_scrape
 import news_scrape
 import blockexplorer_scrape
+
+import os
+import re
+import subprocess
+from datetime import datetime
+
+import openai
+
+# Define the path to the results folder
+RESULTS_DIR = './results'
+
+# Define the system prompt
+system_prompt = (
+    "You are a Bitcoin specialist, investment manager, and financial analyzer. "
+    "You provide professional, insightful, and reasoning-backed analysis of the market "
+    "and trading opportunities."
+)
+
+# Function to run the scraping scripts
+def run_scraping_scripts():
+    subprocess.run(['python', 'price_scrape.py'])
+    subprocess.run(['python', 'news_scrape.py'])
+    subprocess.run(['python', 'blockexplorer_scrape.py'])
+
+# Function to get the latest file with a specific prefix
+def get_latest_file(prefix):
+    files = [f for f in os.listdir(RESULTS_DIR) if re.match(rf'{prefix}_\d+', f)]
+    latest_file = max(files, key=lambda x: datetime.strptime(x[len(prefix) + 1:x.find('.txt')], '%Y%m%d_%H%M%S'))
+    return os.path.join(RESULTS_DIR, latest_file)
+
+# Function to read and title the file contents
+def read_and_title_file(filepath, title):
+    with open(filepath, 'r') as file:
+        content = file.read()
+    return f"{title}:\n{content}\n"
+
+# Main function to run the analysis
+def run_analysis():
+    # Run the scraping scripts
+    run_scraping_scripts()
+    
+    # Get the latest files
+    price_file = get_latest_file('price_data')
+    news_file = get_latest_file('news_analysis')
+    bitcoin_file = get_latest_file('bitcoin_data')
+    
+    # Read and concatenate the file contents with titles
+    concatenated_data = (
+        read_and_title_file(price_file, 'PRICE DATA') +
+        read_and_title_file(news_file, 'NEWS ANALYSIS') +
+        read_and_title_file(bitcoin_file, 'BLOCK EXPLORER DATA')
+    )
+    
+    # Define the analysis prompt
+    analysis_prompt = "Please analyze the following Bitcoin data:\n" + concatenated_data
+    
+    # Create the chat completion
+    response = openai.chat.completions.create(
+        model="gpt-4-1106-preview",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": analysis_prompt}
+        ],
+        temperature=0.3,
+        max_tokens=4000
+    )
+    
+    # Generate a timestamp for the filename
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    filename = f"file_analysis_{timestamp}.txt"
+    filepath = os.path.join(RESULTS_DIR, filename)
+    
+    # Write the response to a file
+    with open(filepath, 'w') as file:
+        file.write(str(response))
+
+    # Print response and the path to the new file
+    print(response)
+    print(f"Analysis saved to: {filepath}")
+
+# Run the main analysis function
+if __name__ == '__main__':
+    run_analysis()
--- news_scrape.py
+++ news_scrape.py
@@ -67,105 +67,24 @@ for article in scraped_articles:
 
 
 
-# Function to analyze an article with GPT-4
-def analyze_article_with_gpt(article_text):
-    try:
-        # System prompt
-        system_prompt = (
-        "You are a highly knowledgeable assistant with expertise in Bitcoin and cryptocurrency markets. "
-        "Your task is to analyze news articles related to Bitcoin. For each article, you will provide a concise summary. "
-        "After summarizing, you will rate the sentiment of the article, its relevance to the Bitcoin and cryptocurrency markets, "
-        "and its overall importance. Rate each of these three aspects on a scale from 0 to 100, where 0 is the lowest and 100 is the highest. "
-        "Your responses should be factual, unbiased, and based solely on the content of the article. "
-        "Respond in a structured format that includes the summary followed by the ratings for sentiment, relevance, and importance. "
-        "For example: 'Summary: [Your summary here]. Sentiment: [0-100], Relevance: [0-100], Importance: [0-100].' "
-        "Do not have any text following the integer rating for Sentiment, Relevance, nor Importance."
-        "Avoid speculation and provide analysis based on the information available in the article."
-        )
+# Function to analyze and store the results for all articles
+def analyze_and_store_articles(articles):
+    for article in articles:
+        analysis_results = analyze_article_with_gpt(article['text'])
+        if analysis_results:
+            article.update(analysis_results)
 
-        # Structured prompt for analysis
-        analysis_prompt = (
-            f"Analyze the following Bitcoin-related article and provide a summary, "
-            f"then rate its sentiment, market relevance, and importance on a scale from 0 to 100. "
-            f"Do not have any text following the integer rating for Sentiment, Relevance, nor Importance."
-            f"We will be parsing the response with a predetermined format, STRICTLY follow the format"
-            f"Respond in the exact following format: "
-            f"Summary: [Your summary here] "
-            f"Sentiment: [0-100], Relevance: [0-100], Importance: [0-100].\n\n{article_text}"
-        )
+# Analyze all articles once and store the results
+analyze_and_store_articles(scraped_articles)
 
-        # Create the chat completion
-        response = openai.chat.completions.create(
-            model="gpt-4-1106-preview",
-            messages=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": analysis_prompt}
-            ],
-            temperature=0.3,
-            max_tokens=1200
-        )
-
-        # Assuming the response follows the format, we extract the summary and ratings
-        response_content = response.choices[0].message.content
-        # print(response.choices[0].message.content)
-        summary = response_content.split('Summary: ')[1].split(' Sentiment: ')[0].strip()
-        
-        # Extracting sentiment, relevance, and importance, handling extra text
-        sentiment_str = response_content.split('Sentiment: ')[1].split(',')[0].split(' ')[0].strip()
-        relevance_str = response_content.split('Relevance: ')[1].split(',')[0].split(' ')[0].strip()
-        importance_str = response_content.split('Importance: ')[1].split('.')[0].split(' ')[0].strip()
-
-        # Converting string to integer, handling any non-numeric characters
-        sentiment = int(''.join(filter(str.isdigit, sentiment_str)))
-        relevance = int(''.join(filter(str.isdigit, relevance_str)))
-        importance = int(''.join(filter(str.isdigit, importance_str)))
-
-        return {
-            'summary': summary,
-            'sentiment': sentiment,
-            'relevance': relevance,
-            'importance': importance
-        }
-    except Exception as e:  # Catching a general exception for simplicity
-        print(f"An error occurred: {str(e)}")
-        return None
-
-# Analyze and rate articles using GPT
+# Now you can print the summaries and ratings from the stored results
 for article in scraped_articles:
-    analysis_results = analyze_article_with_gpt(article['text'])
-    if analysis_results:
-        article.update(analysis_results)
-        print(f"Title: {article['title']}")
-        print(f"Summary: {article['summary']}")
-        print(f"Sentiment Rating: {article['sentiment']}")
-        print(f"Market Relevance Rating: {article['relevance']}")
-        print(f"Importance Rating: {article['importance']}")
-        print("------------------------------------------------\n")
-
-# Parse GPT response for ratings
-def parse_ratings(analysis_text):
-    # Example parsing logic for the structured format
-    ratings = {'sentiment': 0, 'relevance': 0, 'importance': 0}
-    try:
-        # Assuming GPT-4 follows the format, we extract the numbers within brackets
-        ratings['sentiment'] = int(analysis_text.split('Sentiment: [')[1].split(']')[0])
-        ratings['relevance'] = int(analysis_text.split('Relevance: [')[1].split(']')[0])
-        ratings['importance'] = int(analysis_text.split('Importance: [')[1].split(']')[0])
-    except (IndexError, ValueError) as e:
-        print(f"Error parsing ratings: {str(e)}")
-    return ratings
-
-# Print parsed results
-for article in scraped_articles:
-    analysis_results = analyze_article_with_gpt(article['text'])
-    if analysis_results:
-        article.update(analysis_results)
-        print(f"Title: {article['title']}")
-        print(f"Summary: {article['summary']}")
-        print(f"Sentiment Rating: {article['sentiment']}")
-        print(f"Market Relevance Rating: {article['relevance']}")
-        print(f"Importance Rating: {article['importance']}")
-        print("------------------------------------------------\n")
+    print(f"Title: {article['title']}")
+    print(f"Summary: {article['summary']}")
+    print(f"Sentiment Rating: {article['sentiment']}")
+    print(f"Market Relevance Rating: {article['relevance']}")
+    print(f"Importance Rating: {article['importance']}")
+    print("------------------------------------------------\n")
 
 # Function to save results to a file with a timestamp in the filename
 def save_results_to_file(articles, results_directory='results'):
@@ -179,17 +98,13 @@ def save_results_to_file(articles, results_directory='results'):
     
     with open(filename, 'w') as file:
         for article in articles:
-            analysis_results = analyze_article_with_gpt(article['text'])
-            if analysis_results:
-                article.update(analysis_results)
-                file.write(f"Title: {article['title']}\n")
-                file.write(f"Summary: {article['summary']}\n")
-                file.write(f"Sentiment Rating: {article['sentiment']}\n")
-                file.write(f"Market Relevance Rating: {article['relevance']}\n")
-                file.write(f"Importance Rating: {article['importance']}\n")
-                file.write("------------------------------------------------\n\n")
+            file.write(f"Title: {article['title']}\n")
+            file.write(f"Summary: {article['summary']}\n")
+            file.write(f"Sentiment Rating: {article['sentiment']}\n")
+            file.write(f"Market Relevance Rating: {article['relevance']}\n")
+            file.write(f"Importance Rating: {article['importance']}\n")
+            file.write("------------------------------------------------\n\n")
     print(f"Analysis results saved to {filename}")
 
 # Save the results to a file with a timestamped filename
 save_results_to_file(scraped_articles)
-
